7004e04eee593dedd5df2b3f2adc7a32e851d2ce
rtwohey <rtwohey@ionic.io>
2023-11-28 15:11:31 -0500

    demonstrates getKeys throws error on Android

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,12 +1,52 @@
+import { useEffect, useMemo } from 'react';
 import './ExploreContainer.css';
+import {
+  BrowserVault,
+  DeviceSecurityType,
+  IdentityVaultConfig,
+  Vault,
+  VaultType,
+} from '@ionic-enterprise/identity-vault';
+import { Capacitor } from '@capacitor/core';
+import { IonButton } from '@ionic/react';
 
 interface ContainerProps { }
 
+
+const config: IdentityVaultConfig = ({
+  key: 'io.ionic.get-keys',
+  type: VaultType.SecureStorage,
+  deviceSecurityType: DeviceSecurityType.None,
+  lockAfterBackgrounded: 2000,
+  shouldClearVaultAfterTooManyFailedAttempts: true,
+  customPasscodeInvalidUnlockAttempts: 2,
+  unlockVaultOnLoad: false,
+});
+
 const ExploreContainer: React.FC<ContainerProps> = () => {
+
+  const vault = useMemo(() => {
+    return Capacitor.getPlatform() === 'web' ? new BrowserVault(config) : new Vault(config);
+  }, []);
+
+  const setData = async () => {
+    await vault.setValue('uniqueKey', 'data');
+  }
+
+  const getKeys = async () => {
+    const keys = await vault.getKeys();
+    console.log(keys)
+  }
+
+  const clearVault = async () => {
+    await vault.clear();
+  }
+
   return (
     <div id="container">
-      <strong>Ready to create an app?</strong>
-      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      <IonButton onClick={setData}>Set Vault Data</IonButton>
+      <IonButton onClick={getKeys}>Get Keys</IonButton>
+      <IonButton onClick={clearVault}>Clear Vault</IonButton>
     </div>
   );
 };
